14439e466299c23eca0d4cdaaa12d02d4cf3001c
Leonardo <leonardoghibaudo.lg@gmail.com>
2024-11-15 10:17:57 +0100

    Versione 1.0 Versione programmazione sequenziale

---
+++ GhibaudoLeonardoForkJoin/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace GhibaudoLeonardoForkJoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10;
+            int B = 20;
+            int C = 30;
+            int D = A + 5;
+            Console.WriteLine("Il valore di D è: " + D);
+            int E = B * 2;
+            Console.WriteLine("Il valore di E è: " + E);
+            int F = C * C;
+            Console.WriteLine("Il valore di F é: " + F);
+            int G = E - D;
+            Console.WriteLine("Il valore di G é: " + G);
+            int H = F + G;
+            Console.WriteLine("Il valore di H é: " + H);
+        }
+    }
+}
