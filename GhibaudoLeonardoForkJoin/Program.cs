84a5f7be780a0387b1157188bfa1e114b2253af8
Leonardo <leonardoghibaudo.lg@gmail.com>
2024-11-15 10:28:47 +0100

    Versione 2

--- GhibaudoLeonardoForkJoin/Program.cs
+++ GhibaudoLeonardoForkJoin/Program.cs
@@ -5,38 +5,37 @@ class Program
 {
     static void Main()
     {
-        // Inizializzazione delle variabili
+       
         int A = 10;
         int B = 20;
         int C = 30;
 
-        // Task paralleli per il calcolo delle variabili D, E, F
-        Task<int> taskD = Task.Run(() => A + 5);           // Calcola D
-        Task<int> taskE = Task.Run(() => B * 2);           // Calcola E
-        Task<int> taskF = Task.Run(() => C * C);           // Calcola F
+        
+        Task<int> taskD = Task.Run(() => A + 5);          
+        Task<int> taskE = Task.Run(() => B * 2);           
+        Task<int> taskF = Task.Run(() => C * C);           
 
-        // Attendere che tutti i task di D, E e F siano completati
+     
         Task.WhenAll(taskD, taskE, taskF).Wait();
 
-        // Ottenere i risultati
+      
         int D = taskD.Result;
+        Console.WriteLine(D); 
         int E = taskE.Result;
+        Console.WriteLine(E);
         int F = taskF.Result;
+        Console.WriteLine(F);
 
-        // Task per il calcolo di G
-        Task<int> taskG = Task.Run(() => E - D);           // Calcola G
+    
+        Task<int> taskG = Task.Run(() => E - D);   
+        Console.WriteLine(taskG.Result);
 
-        // Calcolo di H dipendente da G e F
+       
         Task<int> taskH = taskG.ContinueWith(prevTask => F + prevTask.Result);
+        Console.WriteLine(taskH.Result); 
 
-        // Attendere che tutti i task siano completati
+      
         taskH.Wait();
 
-        // Stampa i risultati
-        Console.WriteLine(D);  // Risultato di D
-        Console.WriteLine(E);  // Risultato di E
-        Console.WriteLine(F);  // Risultato di F
-        Console.WriteLine(taskG.Result);  // Risultato di G
-        Console.WriteLine(taskH.Result);  // Risultato di H
     }
 }
--- GhibaudoLeonardoForkJoin/obj/Debug/net8.0/GhibaudoLeonardoForkJoin.AssemblyInfo.cs
+++ GhibaudoLeonardoForkJoin/obj/Debug/net8.0/GhibaudoLeonardoForkJoin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GhibaudoLeonardoForkJoin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d04cffe6724d791d24c03d38ff849f82355519")]
 [assembly: System.Reflection.AssemblyProductAttribute("GhibaudoLeonardoForkJoin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GhibaudoLeonardoForkJoin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
